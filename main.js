8bbc272e45157664488b9c08a984c5026b91b3c4
GPA9 <guillempereira@hotmail.com>
2024-04-25 12:00:11 +0200

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+var miNombre = "Guillem";
+let nombreProfe = "David";
+const nombreCurso = "TechTalent";
+
+console.log(typeof miNombre);
+console.log(typeof nombreProfe);
+console.log(typeof nombreCurso);
